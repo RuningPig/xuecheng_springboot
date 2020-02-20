adf2c0e0ba15885e44269cf044cf49acf979bb19
e <837601718@qq.com>
2020-02-20 14:51:09 +0800

    页面预览

--- xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -32,4 +32,10 @@ public interface CmsPageControllerApi {
 
     @ApiOperation("通过ID删除页面")
     public ResponseResult delete(String id);
+
+    @ApiOperation("发布页面")
+    public ResponseResult postPage(String pageId);
+
+    @ApiOperation("取出静态文件")
+    public ResponseResult getHtml(String pageId);
 }
